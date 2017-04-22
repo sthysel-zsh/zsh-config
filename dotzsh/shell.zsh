bd2243d0c7beab4c023c1736d5c4da1dc71766a7
Thys Meintjes <sthysel@gmail.com>
2017-04-22 16:07:35 +0800

    removed zsh extras, all is now either a plugin or in .zshrc

--- dotzsh/shell.zsh
+++
@@ -1,7 +0,0 @@
-bindkey -v
-bindkey -v '^?' backward-delete-char
-
-export PATH=$PATH:$HOME/.local/bin/
-
-bindkey -M viins '^r' history-incremental-search-backward
-bindkey -M vicmd '^r' history-incremental-search-backward
